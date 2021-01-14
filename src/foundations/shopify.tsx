0b8538264af4446a2f35e9bf521586574bfe7763
小俣圭佑 <omatakeisuke@MacBook.local>
2021-01-15 08:15:19 +0900

    Add environment variable

--- src/foundations/shopify.tsx
+++ src/foundations/shopify.tsx
@@ -1,7 +1,7 @@
 import Client from 'shopify-buy'
 
 export const shopify = Client.buildClient({
-  storefrontAccessToken: 'dd4d4dc146542ba7763305d71d1b3d38',
+  storefrontAccessToken: process.env.REACT_APP_ENV_KEY as string,
   domain: 'graphql.myshopify.com',
   language: 'ja-JP',
 })
