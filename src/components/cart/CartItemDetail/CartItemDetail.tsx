96d3f8b7dd6de172fc1a951a77e525f4128715a0
小俣圭佑 <omatakeisuke@MacBook.local>
2021-04-04 19:57:49 +0900

    Fix resetting the cart when old items are in the cart

--- src/components/cart/CartItemDetail/CartItemDetail.tsx
+++ src/components/cart/CartItemDetail/CartItemDetail.tsx
@@ -4,6 +4,7 @@ import { Minus, Plus } from 'components/icons'
 import { useChangeQuantity, useRemoveItem } from 'lib/hooks/cart'
 import { getColorAndSize } from 'lib/helpers'
 import { LineItem } from 'lib/Type'
+import { resetCheckoutId } from 'lib/helpers'
 
 type Props = {
   cartItem: LineItem
@@ -15,44 +16,50 @@ export const CartItemDetail: FC<Props> = ({ cartItem }) => {
 
   return (
     <>
-      <p>{cartItem.title}</p>
-      <div>
-        {/* Color:{' '} */}
-        {getColorAndSize(cartItem.variant.selectedOptions, 'Color')}
-      </div>
-      <div>
-        {/* Size:{' '} */}
-        {getColorAndSize(cartItem.variant.selectedOptions, 'Size')}
-      </div>
-      <p>¥{cartItem.variant.price}</p>
-      <div className="flex">
-        <p>個数：</p>
-        <button
-          aria-label="個数を1つ減らす"
-          onClick={() => ChangeQuantity(cartItem.id, cartItem.quantity - 1)}
-        >
-          <Minus />
-        </button>
-        <div className="mx-3">{cartItem.quantity}</div>
-        <button
-          aria-label="個数を1つ増やす"
-          onClick={() => ChangeQuantity(cartItem.id, cartItem.quantity + 1)}
-        >
-          <Plus />
-        </button>
-      </div>
-      <p>小計： ¥{parseInt(cartItem.variant.price) * cartItem.quantity}</p>
-      <br></br>
-      <div>
-        <Button
-          shape="square"
-          type="button"
-          aria-label="カートから商品を削除する"
-          onClick={() => RemoveItem(cartItem.id)}
-        >
-          削除
-        </Button>
-      </div>
+      {cartItem.variant ? (
+        <>
+          <p>{cartItem.title}</p>
+          <div>
+            {/* Color:{' '} */}
+            {getColorAndSize(cartItem.variant.selectedOptions, 'Color')}
+          </div>
+          <div>
+            {/* Size:{' '} */}
+            {getColorAndSize(cartItem.variant.selectedOptions, 'Size')}
+          </div>
+          <p>{cartItem.variant.price}円</p>
+          <div className="flex">
+            <p>個数：</p>
+            <button
+              aria-label="個数を1つ減らす"
+              onClick={() => ChangeQuantity(cartItem.id, cartItem.quantity - 1)}
+            >
+              <Minus />
+            </button>
+            <div className="mx-3">{cartItem.quantity}</div>
+            <button
+              aria-label="個数を1つ増やす"
+              onClick={() => ChangeQuantity(cartItem.id, cartItem.quantity + 1)}
+            >
+              <Plus />
+            </button>
+          </div>
+          <p>小計： {parseInt(cartItem.variant.price) * cartItem.quantity}円</p>
+          <br></br>
+          <div>
+            <Button
+              shape="square"
+              type="button"
+              aria-label="カートから商品を削除する"
+              onClick={() => RemoveItem(cartItem.id)}
+            >
+              削除
+            </Button>
+          </div>
+        </>
+      ) : (
+        resetCheckoutId()
+      )}
     </>
   )
 }
