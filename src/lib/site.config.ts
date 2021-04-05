d68dfcbbdb48a9d30698681fe6960d4528614af7
小俣圭佑 <omatakeisuke@MacBook.local>
2021-04-05 17:22:40 +0900

    Fix about me page

--- src/lib/site.config.ts
+++ src/lib/site.config.ts
@@ -42,4 +42,14 @@ export const Config = {
     instagram: 'https://twitter.com/mete0la',
     github: 'https://github.com/KeisukeOmata/next_e-commerce',
   },
+  brandName: 'SKPISM',
+  description1:
+    'あらゆるものに縛られ、抑圧される社会と一線を画し、自分自身を取り戻すための服。<br>その人自身のアイデンティティからなる白昼夢のように、持つ人の個性に合わせて表情が変化するアイテムを展開。',
+  description2:
+    '国内外から厳選されたヴィンテージアイテムと、再構築され新たなアイテムとして生まれ変わった一点物を取り扱うVintage Select Bland',
+  description3:
+    '10年後も美しいBAGを目指して使い込むほどに艶を増す上質な本革を使用。<br>MADE IN JAPANにこだわり、優れた職人によって作られたシンプルで長く寄り添う小物を展開。',
+  name: '小俣圭佑',
+  emailAddress: 'omatakeisuke.work@gmail.com',
+  responsibleParty: '小俣圭佑',
 }
