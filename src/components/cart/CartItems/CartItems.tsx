be2079e465aeb8c0fe4d737b934f8f60d8ef3be4
小俣圭佑 <omatakeisuke@MacBook.local>
2021-04-07 20:41:46 +0900

    Change cart items to be displayed in time order

--- src/components/cart/CartItems/CartItems.tsx
+++ src/components/cart/CartItems/CartItems.tsx
@@ -32,12 +32,9 @@ export const CartItems: FC = () => {
                 BAG
               </h2>
             </div>
-            {cart.lineItems
-              .slice(0)
-              .reverse()
-              .map((item, i) => (
-                <CartItem item={item} key={i} />
-              ))}
+            {cart.lineItems.map((item, i) => (
+              <CartItem item={item} key={i} />
+            ))}
             <p className="mt-5 flex flex-col justify-center text-center">
               合計: {cart.subtotalPrice}円
             </p>
