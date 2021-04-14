41ae3b555ce3ef3ca7106e1c5ba789d2c3196e13
小俣圭佑 <omatakeisuke@MacBook.local>
2021-04-15 07:35:59 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -31,7 +31,7 @@ module.exports = withPWA({
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src-elem https://unpkg.com/pwacompat;
+  script-src-elem <https://unpkg.com/pwacompat>;
   script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com *.instagram.com *.shopify.com;
   worker-src 'self' 'unsafe-eval' 'unsafe-inline';
   child-src *.youtube.com *.google.com *.twitter.com *.instagram.com *.shopify.com;
