f083572fcfe14a7f8d0c0e18487798462bc17541
小俣圭佑 <omatakeisuke@MacBook.local>
2021-04-17 17:08:01 +0900

    Add test

--- src/lib/shopify.ts
+++ src/lib/shopify.ts
@@ -1,7 +1,7 @@
 import Client from 'shopify-buy'
 
 export const shopify = Client.buildClient({
-  storefrontAccessToken: process.env.NEXT_PUBLIC_SHOPIFY_API_TOKEN,
+  storefrontAccessToken: process.env.NEXT_PUBLIC_SHOPIFY_API_TOKEN as string,
   domain: `${process.env.NEXT_PUBLIC_SHOPIFY_STORE_ID}.myshopify.com`,
   language: 'ja-JP',
 })
