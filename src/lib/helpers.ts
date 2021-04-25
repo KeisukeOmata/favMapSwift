6e81fc742b451ba42550b2c39b76c92a7e68d1d4
小俣圭佑 <omatakeisuke@MacBook.local>
2021-04-26 08:35:38 +0900

    Change helper functions to arrow function

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,55 +1,55 @@
 import { SelectedOption, Cart } from 'lib/Type'
 
-export function getCheckoutId(): string {
+export const getCheckoutId = (): string => {
   return localStorage.getItem('checkoutId') ?? ''
 }
 
-export function setCheckoutId(checkoutId: string | number): void {
+export const setCheckoutId = (checkoutId: string | number): void => {
   localStorage.setItem('checkoutId', String(checkoutId))
 }
 
 // Delete checkoutId from local storage
-export function resetCheckoutId(): void {
+export const resetCheckoutId = (): void => {
   localStorage.removeItem('checkoutId')
 }
 
-export function getColorAndSize(
+export const getColorAndSize = (
   options: SelectedOption[],
   name: string
-): string {
+): string => {
   return options.find((option) => option.name === name)?.value ?? ''
 }
 
-export function presenceCheck(
+export const presenceCheck = (
   cartState: Cart,
   itemIdState: string | number
-): string {
+): string => {
   return (
     cartState.lineItems.find((lineItem) => lineItem.variant.id === itemIdState)
       ?.variant.id ?? ''
   )
 }
 
-export function getCartItemId(
+export const getCartItemId = (
   cartState: Cart,
   itemIdState: string | number
-): string {
+): string => {
   return (
     cartState.lineItems.find((lineItem) => lineItem.variant.id === itemIdState)
       ?.id ?? ''
   )
 }
 
-export function getCurrentQuantity(
+export const getCurrentQuantity = (
   cartState: Cart,
   itemIdState: string | number
-): number {
+): number => {
   return (
     cartState.lineItems.find((lineItem) => lineItem.variant.id === itemIdState)
       ?.quantity ?? 0
   )
 }
 
-export function getItemPath(id: string): string {
+export const getItemPath = (id: string): string => {
   return `/items/${encodeURIComponent(id)}`
 }
