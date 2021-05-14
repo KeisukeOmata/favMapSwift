246ded136d122faad2c038fbe07ca91f4905c720
小俣圭佑 <omatakeisuke@MacBook.local>
2021-05-14 19:04:50 +0900

    Fix Can't perform a React state update on an unmounted component

--- src/lib/hooks/state/useLoading.ts
+++ src/lib/hooks/state/useLoading.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useEffect, useRef, useCallback } from 'react'
 
 type useLoadingType = {
   loading: boolean
@@ -7,9 +7,20 @@ type useLoadingType = {
 
 export const useLoading = (): useLoadingType => {
   const [loading, setLoadingState] = useState<boolean>(false)
+  const mountedRef = useRef(false)
+
+  useEffect(() => {
+    mountedRef.current = true
+    return () => {
+      mountedRef.current = false
+    }
+  })
 
   const setLoading = useCallback((loading: boolean): void => {
-    setLoadingState(loading)
+    // Check to see if it is still mounted
+    if (mountedRef.current) {
+      setLoadingState(loading)
+    }
   }, [])
 
   return {
