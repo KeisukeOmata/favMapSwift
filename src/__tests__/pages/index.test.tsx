50351e0a024ec9c5835e0c360c5d839ac6fb843d
小俣圭佑 <omatakeisuke@MacBook.local>
2021-05-28 13:22:15 +0900

    Add test for ColorAndSize component

--- src/__tests__/pages/index.test.tsx
+++
@@ -1,7 +0,0 @@
-import Home from 'pages/index'
-// Avoid conflicts with cypress
-import { expect } from '@jest/globals'
-
-it('The Home component is present', () => {
-  expect(Home).toBeTruthy()
-})
