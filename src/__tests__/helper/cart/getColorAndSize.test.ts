e071bc9da12aa6b6fbdcdb5806a814965a464e18
小俣圭佑 <omatakeisuke@MacBook.local>
2021-06-09 08:40:18 +0900

    Add test for getColorAndSize helper

---
+++ src/__tests__/helper/cart/getColorAndSize.test.ts
@@ -0,0 +1,24 @@
+import { getColorAndSize } from 'lib/helper/cart'
+import { SelectedOption } from 'lib/Type'
+
+describe('getColorAndSize', () => {
+  let option: SelectedOption[]
+  beforeAll(() => {
+    option = [
+      { name: 'Color', value: 'Black' },
+      { name: 'Size', value: 'S' },
+    ]
+  })
+  test('Color', () => {
+    const color = getColorAndSize(option, 'Color')
+    expect(color).toBe('Black')
+  })
+  test('Size', () => {
+    const size = getColorAndSize(option, 'Size')
+    expect(size).toBe('S')
+  })
+  test('Other', () => {
+    const other = getColorAndSize(option, '')
+    expect(other).toBe('')
+  })
+})
