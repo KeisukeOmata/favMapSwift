7e7f829d46f40a9cd81619265f789d211e6bd719
小俣圭佑 <omatakeisuke@MacBook.local>
2021-07-15 21:18:00 +0900

    Fix Goole Analytics

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -2,7 +2,9 @@ export const gooleAnalyticsId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || '
 
 export const pageview = (url: string): void => {
   if (!gooleAnalyticsId) return
-  window.gtag('config', gooleAnalyticsId, {
-    page_path: url,
-  })
+  if (typeof window !== 'undefined') {
+    window.gtag('config', gooleAnalyticsId, {
+      page_path: url,
+    })
+  }
 }
